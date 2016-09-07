e1d72c5453d3d05cee62da3279ed31259013b455
Nathaniel Cotton <ncotton6@gmail.com>
2016-09-07 08:26:47 -0400

    commenting

--- src/main/java/model/ByteArray.java
+++ src/main/java/model/ByteArray.java
@@ -1,10 +1,21 @@
 package model;
 
 /**
+ * File: ByteArray.java
  * Created by nathaniel on 8/29/16.
+ *
+ * All classes that implement ByteArray will provide a mechanism to turn the underlying
+ * object into a byte[].  This can be easily used to transmit data across a network.
  */
 public interface ByteArray {
 
+    /**
+     * A call to this method on an implementing class will turn the object into
+     * a byte[]. This will make it easier for transmitting the data across a
+     * network.
+     *
+     * @return byte[] representing the object.
+     */
     byte[] getByteArray();
 
 }
